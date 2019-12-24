854f891cff0cfe4519bd9959d4175d280e6147e6
JosephCollins Udeani <JosephCollins.Udeani@aa.com>
2019-12-23 23:32:19 -0600

    added register page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,20 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
+export class HomePage implements OnInit {
+
   menuData = [
     { title: 'Our Menu', pic: 'assets/img/soup1.jpg', pushPage: 'MenuPage' },
     { title: 'Account', pic: 'assets/img/coffee-people3.jpg', pushPage: 'AccountPage' },
     { title: 'About Us', pic: 'assets/img/coffee6.jpg', pushPage: 'AboutPage' },
-    { title: 'Locations', pic: 'assets/img/cafe2.jpg', pushPage: 'LocationsPage' },
+    { title: 'Locations', pic: 'assets/img/cafe2.jpg', pushPage: 'LocationsPage' }
+  ];
 
-  ]
+  logPage: any;
+
   constructor(public navCtrl: NavController) {
 
+  }
+
+  ngOnInit(): void {
+    this.logPage = 'LoginPage';
   }
 
 }
