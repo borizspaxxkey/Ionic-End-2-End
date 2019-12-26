dd7d7a6efd928b532b0c5792a586e3eff96cfd70
JosephCollins Udeani <JosephCollins.Udeani@aa.com>
2019-12-26 09:42:19 -0600

    added menu detail page

--- src/pages/menu-detail/menu-detail.ts
+++ src/pages/menu-detail/menu-detail.ts
@@ -1,25 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the MenuDetailPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { MenuServiceProvider } from '../../providers/menu-service/menu-service';
 
 @IonicPage()
 @Component({
   selector: 'page-menu-detail',
   templateUrl: 'menu-detail.html',
 })
-export class MenuDetailPage {
+export class MenuDetailPage implements OnInit {
+  theCoffee = {
+    id: '',
+    name: '',
+    description: '',
+    img: '',
+    small: 0,
+    medium: 0,
+    large: 0,
+    size: '',
+    price: 0,
+    milk: 'none',
+    whip: 'none'
+  };
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController,
+    public navParams: NavParams,
+    private menuService: MenuServiceProvider) {
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad MenuDetailPage');
+  ngOnInit(): void {
+    let id = this.navParams.get('id');
+    this.menuService.getOne(id)
+      .then((ret) => {
+        this.theCoffee = { ...ret }
+      });
   }
 
+  addToCart() {
+    if (this.theCoffee.price == this.theCoffee.small) {
+      this.theCoffee.size = 'small';
+    }
+    else if (this.theCoffee.price == this.theCoffee.medium) {
+      this.theCoffee.size = 'medium';
+    }
+    else {
+      this.theCoffee.size = 'large';
+    }
+
+    console.log('clicked', this.theCoffee);
+  }
 }
